0f28793b4414ab183c443bf5858a80cf5e80d0bd
Yadhu <yadhu621@gmail.com>
2023-02-15 12:23:23 +0000

    Initial commit

---
+++ heapmate/src/main/java/com/examplelab/heapmate/HeapmateApplication.java
@@ -0,0 +1,13 @@
+package com.examplelab.heapmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeapmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeapmateApplication.class, args);
+	}
+
+}
