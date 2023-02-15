2d284d9a01fb1b1b5872c59ac0bb90f825ee8f4f
Yadhu <yadhu621@gmail.com>
2023-02-15 12:47:16 +0000

    add /memory/allocate endpoint

--- heapmate/src/main/java/com/examplelab/heapmate/controller/MemoryController.java
+++ heapmate/src/main/java/com/examplelab/heapmate/controller/MemoryController.java
@@ -1,14 +1,22 @@
 package com.examplelab.heapmate.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemoryController {
+  
   @GetMapping("/memory")
   public long getMemoryUsage() {
     Runtime runtime = Runtime.getRuntime();
     long currentMemoryUsage = (runtime.totalMemory() - runtime.freeMemory()) / 1024 / 1024;
     return currentMemoryUsage;
+  }
+
+  @PostMapping("/memory/allocate")
+  public void allocateMemory(@RequestParam("value") long value) {
+    byte[] bytes = new byte[(int) value * 1024 * 1024];
   }
 }
